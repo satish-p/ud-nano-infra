636b39f516c89f94498d5f69bec55f506309a76e
Satish <stateofmind@satish.local>
2020-07-24 11:25:51 -0700

    adding missing files

--- Task6/Exercise_2/outputs.tf
+++ Task6/Exercise_2/outputs.tf
@@ -1,1 +1,5 @@
-# TODO: Define the output variable for the lambda function.
+# Define the output variable for the lambda function.
+
+output "lambda_function_arn" {
+  value = "${aws_lambda_function.lambda_greeting.arn}"
+}# TODO: Define the output variable for the lambda function.
--- Task6/Exercise_2/variables.tf
+++ Task6/Exercise_2/variables.tf
@@ -1,1 +1,8 @@
-# TODO: Define the variable for aws_region
+# Define the variable for aws_region
+
+variable "aws_region" {
+  type        = string
+  description = "region to deploy labda"
+  default     = "us-east-1"
+}
+
